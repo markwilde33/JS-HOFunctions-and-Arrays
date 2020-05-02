ed801af238d1a0cb6b64e6a4e60aa09b8a69cc24
Mark Wilde <markwilde29@gmail.com>
2020-05-02 16:15:49 +0100

    Compared a standard for loop, which uses an if/else statement; with the filter method.

--- main.js
+++ main.js
@@ -56,6 +56,7 @@ const companies = [{
 
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
+/*
 // standard for loop
 for (let i = 0; i < companies.length; i++) {
    console.log(companies[i]);
@@ -68,8 +69,52 @@ companies.forEach(function (company) {
 
 // ES6 syntax
 companies.forEach(company => console.log(company));
+*/
 
 // filter
+
+// standard logic, set empty arrays, use for loop, push to arrays
+let canMarry = [];
+let cantMarry = [];
+for (let i = 0; i < ages.length; i++) {
+   if (ages[i] >= 18) {
+      canMarry.push(ages[i])
+   } else {
+      cantMarry.push(ages[i])
+   }
+}
+
+console.log(canMarry);
+console.log(cantMarry);
+
+// filter method
+const canMarry2 = ages.filter(function (age) {
+   if (age >= 18) {
+      return true;
+   }
+});
+
+console.log(canMarry2);
+
+const cantMarry2 = ages.filter(function (age) {
+   if (age <= 18) {
+      return true;
+   }
+});
+
+console.log(cantMarry2);
+
+// ES6 syntax
+const canMarry3 = ages.filter(age => age >= 18);
+
+console.log(canMarry3);
+
+const cantMarry3 = ages.filter(age => age <= 18);
+
+console.log(cantMarry3);
+
+
+
 // map
 // sort
 // reduce
